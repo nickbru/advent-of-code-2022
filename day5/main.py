cc48b168b4a3b4158830f967cd02f12da86e9cc1
Nicholas <nickbru1991@gmail.com>
2022-12-06 21:15:58 -0600

    day 5 solution w/ coworkers :)

--- day5/main.py
+++ day5/main.py
@@ -1,7 +1,60 @@
-data = open("sample.txt", "r").readlines()
-data = [d.replace("\n","") for d in data]
-print(data)
+data = []
+total = 0
 
-stack_data = data
-print(stack_data)
-stack_data = [l.replace("    ","-") for l in stack_data]
+crates_finished = False
+crate_lines = []
+crate_stacks = []
+movements = 0
+
+for l in open('puzzle_input.txt'):
+    l = l.replace('\n', '')
+
+    if l:
+        if not crates_finished:
+            crate_line = l[1::4]
+            if not crate_line[0] == '1':
+                # Crate data
+                crate_lines.append(list(crate_line))
+            else:
+                for _ in range(len(crate_line)):
+                    crate_stacks.append(list())
+
+                # Parse and format
+                crate_lines.reverse()
+
+                for line in crate_lines:
+                    for i, crate in enumerate(line):
+                        if crate != ' ':
+                            crate_stacks[i].append(crate)
+        else:
+            move_data = l.split(' ')
+            quantity = int(move_data[1])
+            origin = int(move_data[3])
+            destination = int(move_data[5])
+
+            movements += quantity
+# Generate Solution for part 1
+#            for _ in range(quantity):
+#                move_crate = crate_stacks[origin - 1].pop()
+#                crate_stacks[destination - 1].append(move_crate)
+# Generate Solution for part 2
+            length = len(crate_stacks[origin - 1])
+            for _ in range(quantity):
+                move_crate=crate_stacks[origin - 1].pop(length-quantity)
+                crate_stacks[destination - 1].append(move_crate)
+
+    else:
+        crates_finished = True
+
+
+print(crate_stacks)
+
+answer = ''
+
+for stack in crate_stacks:
+    answer += stack[-1]
+
+
+print(answer)
+
+print('moves:', movements)
--- day5/readme.md
+++
@@ -1,5 +0,0 @@
-Day 5: Supply Stacks.
-
-Need to pull in each of the stacks and order of the stacks.
-Then need to pull in each of moves 
-
